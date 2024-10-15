05d0bed5c6201abe371b12f24c04de994144558f
LENOVO <aruntendulkar3008@gmail.com>
2024-10-14 22:05:18 -0700

    check

---
+++ src/Oct/exe_02102024_Main_Method/Lab001.java
@@ -0,0 +1,8 @@
+package Oct.exe_02102024_Main_Method;
+
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/Oct/exe_02102024_Main_Method/Lab002.java
@@ -0,0 +1,4 @@
+package Oct.exe_02102024_Main_Method;
+
+public class Lab002 {
+}
