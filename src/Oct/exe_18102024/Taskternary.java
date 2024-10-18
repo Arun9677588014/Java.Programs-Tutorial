67af12b8dc9c1e7236ee8117d1d1a203cd6d40ed
LENOVO <aruntendulkar3008@gmail.com>
2024-10-17 23:29:53 -0700

    check

---
+++ src/Oct/exe_18102024/Taskternary.java
@@ -0,0 +1,10 @@
+package Oct.exe_18102024;
+
+public class Taskternary {
+    public static void main(String[] args) {
+        int Arun_age = 21;
+        int Emilla_age = 36;
+        String result = Arun_age >= Emilla_age ? "Arun is older than Emilla " : "Emilla is older than Arun ";
+        System.out.println(result);
+    }
+}
