b5397ac2f11d47999527ab2b6f79eb4acc76ba4c
LENOVO <aruntendulkar3008@gmail.com>
2024-10-20 23:33:44 -0700

    Task Arguments

---
+++ src/Oct/exe_03102024/trail.java
@@ -0,0 +1,14 @@
+package Oct.exe_03102024;
+
+public class trail {
+    public static void main(String[] args) {
+        boolean a = true ;
+        boolean b = true ;
+        boolean c = a || b;
+
+        System.out.println((c));
+    }
+
+
+
+}
